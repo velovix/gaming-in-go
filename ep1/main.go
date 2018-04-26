722b96d8b4a32d34b7161f2f16c2a582d7e8be26
Tyler Compton <xaviosx@gmail.com>
2018-04-25 21:51:51 -0700

    Add episode 1 code

--- ep1/main.go
+++ ep1/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -12,30 +11,11 @@ const (
 	screenHeight = 800
 )
 
-var deltaTime = 1.0 / 60.0
-
-func textureFromBMP(renderer *sdl.Renderer, filename string) *sdl.Texture {
-	// Initialize the sprite as a texture
-	img, err := sdl.LoadBMP(filename)
-	if err != nil {
-		panic(fmt.Sprintf("loading %v: %v", filename, err))
-	}
-	defer img.Free()
-
-	tex, err := renderer.CreateTextureFromSurface(img)
-	if err != nil {
-		panic(fmt.Sprintf("creating texture for %v: %v", filename, err))
-	}
-
-	return tex
-}
-
 func main() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		fmt.Println("initializing SDL:", err)
 		return
 	}
-	defer sdl.Quit()
 
 	window, err := sdl.CreateWindow(
 		"Gaming in Go Episode 2",
@@ -43,72 +23,37 @@ func main() {
 		screenWidth, screenHeight,
 		sdl.WINDOW_OPENGL)
 	if err != nil {
-		fmt.Println("creating window:", err)
+		fmt.Println("initializing window:", err)
 		return
 	}
 	defer window.Destroy()
 
 	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
-		fmt.Println("creating renderer:", err)
+		fmt.Println("initializing renderer:", err)
 		return
 	}
 	defer renderer.Destroy()
 
-	player := newPlayer(renderer)
-	defer player.destroy()
-
-	bg := newBackground(renderer)
-	defer bg.destroy()
-
-	const (
-		waveWidth  = 5
-		waveHeight = 3
-	)
-	var enemyWave []basicEnemy
-	for i := 0; i < waveWidth; i++ {
-		for j := 0; j < waveHeight; j++ {
-			x := (float64(i)/waveWidth)*screenWidth + enemySize/2
-			y := float64(enemySize*j) + enemySize/2
-
-			e := newBasicEnemy(renderer, x, y)
-			defer e.destroy()
-			enemyWave = append(enemyWave, e)
-		}
+	plr, err := newPlayer(renderer)
+	if err != nil {
+		fmt.Println("creating player:", err)
+		return
 	}
 
-	initBulletRegistry(renderer)
-
-	quitting := false
-	for !quitting {
-		startTime := time.Now()
-
+	for {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
 			case *sdl.QuitEvent:
-				quitting = true
+				return
 			}
 		}
 
 		renderer.SetDrawColor(255, 255, 255, 255)
 		renderer.Clear()
 
-		bg.draw(renderer)
-
-		player.update()
-		player.draw(renderer)
-
-		for _, enemy := range enemyWave {
-			enemy.draw(renderer)
-		}
-
-		for _, bul := range bulletRegistry {
-			bul.draw(renderer)
-			bul.update()
-		}
+		plr.draw(renderer)
 
 		renderer.Present()
-
-		deltaTime = time.Since(startTime).Seconds()
 	}
 }
--- ep1/player.go
+++ ep1/player.go
@@ -2,82 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-const (
-	playerSize  = 105
-	playerSpeed = 240
-
-	playerShotDelay = time.Millisecond * 300
-)
-
-// player represents the player character.
 type player struct {
-	tex  *sdl.Texture
-	x, y float64
-
-	lastShot time.Time
+	tex *sdl.Texture
 }
 
-// newPlayer creates a new player object, loading all sprites.
-func newPlayer(renderer *sdl.Renderer) (p player) {
-	p.tex = textureFromBMP(renderer, "sprites/player.bmp")
+func newPlayer(renderer *sdl.Renderer) (p player, err error) {
+	img, err := sdl.LoadBMP("sprites/player.bmp")
+	if err != nil {
+		return player{}, fmt.Errorf("loading player sprite: %v", err)
+	}
+	defer img.Free()
+	p.tex, err = renderer.CreateTextureFromSurface(img)
+	if err != nil {
+		return player{}, fmt.Errorf("creating player texture: %v", err)
+	}
 
-	// Position the player at the bottom middle of the screen
-	p.x = screenWidth / 2.0
-	p.y = screenHeight - (playerSize / 2.0)
-
-	return p
+	return p, nil
 }
 
 func (p *player) draw(renderer *sdl.Renderer) {
-	// Convert points to refer to top left of sprite
-	x := p.x - (playerSize / 2.0)
-	y := p.y - (playerSize / 2.0)
-
 	renderer.Copy(p.tex,
-		&sdl.Rect{X: 0, Y: 0, W: playerSize, H: playerSize},
-		&sdl.Rect{X: int32(x), Y: int32(y), W: playerSize, H: playerSize})
-}
-
-func (p *player) update() {
-	keys := sdl.GetKeyboardState()
-
-	if keys[sdl.SCANCODE_LEFT] == 1 {
-		if p.x-playerSize/2.0 > 0 {
-			p.x -= playerSpeed * deltaTime
-		}
-	} else if keys[sdl.SCANCODE_RIGHT] == 1 {
-		if p.x+playerSize/2.0 < screenWidth {
-			p.x += playerSpeed * deltaTime
-		}
-	}
-
-	if keys[sdl.SCANCODE_SPACE] == 1 {
-		if time.Since(p.lastShot) >= playerShotDelay {
-			p.shoot(p.x+25, p.y-20)
-			p.shoot(p.x-25, p.y-20)
-
-			p.lastShot = time.Now()
-		}
-	}
-}
-
-func (p *player) shoot(x, y float64) {
-	if bul, ok := fromBulletRegistry(); ok {
-		bul.x = x
-		bul.y = y
-		bul.angle = 270 * (math.Pi / 180.0)
-		bul.active = true
-	} else {
-		fmt.Println("Warning: Ran out of bullets in registry")
-	}
-}
-
-func (p *player) destroy() {
-	p.tex.Destroy()
+		&sdl.Rect{X: 0, Y: 0, W: 105, H: 105},
+		&sdl.Rect{X: 40, Y: 20, W: 105, H: 105})
 }
